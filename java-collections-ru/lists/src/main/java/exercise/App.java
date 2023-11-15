44b732d81a37aeff117d96e8c363c39674816c89
leomaks <s.arkhin@gmail.com>
2023-11-15 07:35:25 +0200

    list

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,26 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+class App {
+    // BEGIN
+    public static boolean scrabble(String text, String word) {
+
+        var tmpText = new ArrayList<Character>();
+        for (var ch : text.toCharArray()) {
+            tmpText.add(ch);
+        }
+
+
+        for (var letter : word.toLowerCase().toCharArray()) {
+            var index = tmpText.indexOf(letter);
+            if (index == -1) {
+             return false;
+            }
+            tmpText.remove(index);
+        }
+        return true;
+    }
+}
+//END
