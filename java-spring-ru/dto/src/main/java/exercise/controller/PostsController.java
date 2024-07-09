bf4bb486f8e94bd9339d01aa4018e4148bee62a2
@hexlet/cli <support@hexlet.io>
2024-07-09 09:07:47 +0200

    submit java-spring-ru/dto

---
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,72 @@
+package exercise.controller;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.exception.ResourceNotFoundException;
+import exercise.dto.PostDTO;
+import exercise.dto.CommentDTO;
+
+// BEGIN
+
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping(path = "")
+    public List<PostDTO> index() {
+
+        return postRepository.findAll().stream()
+                .map(this::postToDTO)
+                .toList();
+    }
+
+    @GetMapping(path = "/{id}")
+    public PostDTO show(@PathVariable long id) {
+
+        var post =  postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+
+        return postToDTO(post);
+    }
+
+
+
+    private CommentDTO commentToDTO(Comment comment) {
+        var dto = new CommentDTO();
+        dto.setId(comment.getId());
+        dto.setBody(comment.getBody());
+        return dto;
+    }
+
+    private PostDTO postToDTO(Post post) {
+
+        var dto = new PostDTO();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setBody(post.getBody());
+
+        var comments = commentRepository.findByPostId(post.getId());
+        var result = comments.stream()
+                .map(this::commentToDTO)
+                .toList();
+        dto.setComments(result);
+
+        return dto;
+    }
+
+}
+
+// END
