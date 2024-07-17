112aafa3d4e86c4874735d0424ba0c7a8ce9621d
@hexlet/cli <support@hexlet.io>
2024-07-17 13:28:24 +0200

    submit java-spring-ru/service-layer

---
+++ java-spring-ru/service-layer/src/main/java/exercise/service/BookService.java
@@ -0,0 +1,57 @@
+package exercise.service;
+
+import exercise.dto.BookCreateDTO;
+import exercise.dto.BookDTO;
+import exercise.dto.BookUpdateDTO;
+import exercise.exception.ResourceNotFoundException;
+import exercise.mapper.BookMapper;
+import exercise.repository.AuthorRepository;
+import exercise.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    // BEGIN
+    @Autowired
+    private BookRepository repository;
+    @Autowired
+    private AuthorRepository authorRepository;
+    @Autowired
+    private BookMapper bookMapper;
+
+    public List<BookDTO> getAll() {
+        var books = repository.findAll();
+        return books.stream()
+                .map(bookMapper::map)
+                .toList();
+    }
+
+    public BookDTO findById(Long id) {
+        var book = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not Found: " + id));
+        return bookMapper.map(book);
+    }
+
+    public BookDTO create(BookCreateDTO bookData) {
+        var book = bookMapper.map(bookData);
+        repository.save(book);
+        return bookMapper.map(book);
+
+    }
+    public BookDTO update(BookUpdateDTO bookData, Long id) {
+        var book = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not Found"));
+        bookMapper.update(bookData, book);
+        repository.save(book);
+        return bookMapper.map(book);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    // END
+}
